34738ba9638d1edb34c217a4bd48e0771d40fc00
Daryl Ramirez <darylc.ramirez@gmail.com>
2022-01-04 22:37:16 -0500

    introduction section

--- src/components/introduction/Intro.jsx
+++ src/components/introduction/Intro.jsx
@@ -1,4 +1,5 @@
 import'./Intro.css'
+import Me from '../../img/me.png'
 
 const Intro = () => {
     return (
@@ -20,13 +21,71 @@ const Intro = () => {
         </div>
         </div>
     </div>
-    <div className='i-description'>
+    <p className='i-description'>
 Thanks for taking a look around my portfolio website! I am a former public school math teacher and current web developer. I love that coding allows me to learn and practice new things every day. I'm currently working on new projects to expand my skillset. When I'm not coding, you can find me cooking and creating new plant-based recipes or practicing yoga.
-    </div>
+    </p>
 </div>
+<svg
+          width="75"
+          height="75"
+          viewBox="0 0 75 75"
+          fill="none"
+          stroke="black"
+          className="i-scroll"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="scroll">
+            <path
+              id="Vector"
+              d="M40.5 15L34.5 9L28.5 15"
+              stroke-width="3"
+              stroke-miterlimit="10"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              id="Vector_2"
+              d="M28.5 24L34.5 30L40.5 24"
+              stroke-width="3"
+              stroke-miterlimit="10"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <g id="Group">
+              <path
+                id="Vector_3"
+                d="M9 37.5H60"
+                stroke-width="3"
+                stroke-miterlimit="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+            <path
+              id="Vector_4"
+              d="M34.5 27V9"
+              stroke-width="2.9895"
+              stroke-miterlimit="10"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <g id="Group_2">
+              <path
+                id="Vector_5"
+                d="M9 27C9 12.918 20.418 1.5 34.5 1.5C48.5859 1.5 60 12.918 60 27C60 29.8906 60 45.1094 60 48C60 62.082 48.5859 73.5 34.5 73.5C20.418 73.5 9 62.082 9 48C9 45.1094 9 29.8906 9 27Z"
+                stroke-width="3"
+                stroke-miterlimit="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+          </g>
+        </svg>
 </div>
-<div className='i-right'>right</div>
-{/* <img src='' alt='' className='i-img'/> */}
+<div className='i-right'>
+<div className="i-bg"></div>
+<img src={Me} alt='A bitmoji of me.' className='i-img'/>
+</div>
         </div>
     )
 }
