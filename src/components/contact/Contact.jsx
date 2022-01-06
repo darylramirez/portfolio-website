d75507b6c53ffbd353a8ac6a0091d694ce961315
Daryl Ramirez <darylc.ramirez@gmail.com>
2022-01-05 19:20:02 -0500

    (feat): contact form and links

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,31 @@
 import './Contact.css'
+import email from '../../img/email.png'
+import address from '../../img/address.png'
+import project from '../../img/project.png'
+import linkedin from '../../img/linkedin.png'
+import github from '../../img/github.png'
+import resume from '../../img/resume.png'
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 
 
 const Contact = () => {
+    const formRef = useRef();
+    const [done, setDone] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(false)
+
+    const handleSubmit =(e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_j1nbo2w', 'template_xj0ucga', formRef.current, 'user_3imhghXh3yOtC2KKSpYyf')
+      .then((result) => {
+          console.log(result.text);
+          setDone(true);
+      }, (error) => {
+          console.log(error.text);
+          setErrorMessage(true)
+      });
+    }
     return (
         <div className='c'>
             <div className='c-bg'></div>
@@ -10,11 +34,47 @@ const Contact = () => {
             <h1 className='c-title'>Let's chat!</h1>
             <div className='c-info'>
             <div className='c-info-item'>
-                <img src='' alt='' className='c-icon'/>
+                <a href='mailto:darylc.ramirez@gmail.com' title='email' target='_blank' rel="noreferrer">
+                <img src={email} alt='' className='c-icon'/>
+                darylc.ramirez@gmail.com
+                </a>
+                </div>
+                <div className='c-info-item'>
+                <img src={address} alt='' className='c-icon'/>
+                New York, NY, USA
+                </div>
+                <div className='c-info-item'>
+                <a href='https://www.linkedin.com/in/darylramirez/' title='linkedin' target='_blank' rel="noreferrer">
+                <img src={linkedin} alt='' className='c-icon'/>
+                </a>
+                </div>
+                <div className='c-info-item'>
+                <a href='https://github.com/darylramirez/' title='github' target='_blank' rel="noreferrer">
+                <img src={github} alt='' className='c-icon'/>
+                </a>
+                </div>
+                <div className='c-info-item'>
+                <a href='https://darylramirez507609012.wordpress.com/my-resume/' title='resume' target='_blank' rel="noreferrer">
+                <img src={resume} alt='' className='c-icon'/>
+                My resume
+                </a>
                 </div>
             </div>
             </div>
             <div className='c-right'>
+            <p className="c-desc">
+            <b>Get in touch!</b>
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+              <input type='text' placeholder='Name' name='username'></input>
+              <input type='text' placeholder='Subject' name='user_subject'></input>
+              <input type='text' placeholder='Email' name='user_email'></input>
+              <textarea rows='5' placeholder='Message' name='message'/>
+              <button>Submit</button>
+              <div className='success'>
+              {done ? 'I got your message!' : (errorMessage) ? 'Something went wrong!' : null}
+              </div>
+          </form>
         </div>
         </div>
         </div>
