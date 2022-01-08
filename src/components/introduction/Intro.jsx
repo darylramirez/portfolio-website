e4276be771d9e79b200084ca6a922502e493d89b
Daryl Ramirez <darylc.ramirez@gmail.com>
2022-01-08 00:45:15 -0500

    (feat): scroll icon

--- src/components/introduction/Intro.jsx
+++ src/components/introduction/Intro.jsx
@@ -25,7 +25,7 @@ const Intro = () => {
 Thanks for taking a look around my portfolio website! I am a former public school math teacher and current web developer. I love that coding allows me to learn and practice new things every day. I'm currently working on new projects to expand my skillset. When I'm not coding, you can find me cooking and creating new plant-based recipes or practicing yoga.
     </p>
 </div>
-<svg
+{/* <svg
           width="75"
           height="75"
           viewBox="0 0 75 75"
@@ -80,8 +80,13 @@ Thanks for taking a look around my portfolio website! I am a former public schoo
               />
             </g>
           </g>
-        </svg>
-</div>
+        </svg> */}
+       <svg className="arrows">
+              <path className="a1" d="M0 0 L30 32 L60 0"></path>
+              <path className="a2" d="M0 20 L30 52 L60 20"></path>
+              <path className="a3" d="M0 40 L30 72 L60 40"></path>
+            </svg>
+            </div>
 <div className='i-right'>
 <div className="i-bg"></div>
 <img src={Me} alt='A bitmoji of me.' className='i-img'/>
